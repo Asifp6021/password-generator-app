c32009448cc5c0e50c2fe9332cefb0dc4ee9b94c
Asif-patel <asifp6021@gmail.com>
2023-10-21 13:53:36 +0530

    3 -> created form submit event listener and stored generatePassword() function in it

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+
+// range and number input elements
 const rangeCharacters = document.getElementById('range-char');
 
 const numberCharacters = document.getElementById('number-char');
+
+// form submit element
+const formContainer = document.querySelector('#password-form');
+
+//number of char, symbols, number, upperCase elements
+const numbersEl = document.querySelector('#numbers');
+const symbolsEl = document.querySelector('#symbols');
+const upperCaseEl = document.querySelector('#uppercase');
+
 
 
 //Synchronizing Range and Number Inputs
@@ -13,3 +24,22 @@ function syncCharAmount(e) {
     rangeCharacters.value = ValueAmount;
     numberCharacters.value = ValueAmount;
 };
+
+//Generating the password when the form is submitted
+
+formContainer.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const characterAmount = numberCharacters.value;
+    const includeUpperCase = upperCaseEl.value;
+    const includeNumbers = numbersEl.value;
+    const includeSymbols = symbolsEl.value;
+
+    const password = generatePassword(
+
+        characterAmount,
+        includeUpperCase,
+        includeNumbers,
+        includeSymbols
+    )
+})
