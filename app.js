715e8c25fe733960e80b1ad01b01c4e247cde8a2
Asif-patel <asifp6021@gmail.com>
2023-10-21 15:54:43 +0530

    4 -> created ascci code and saved it within variable and created arrayLowToHigh() function to loop over codes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // range and number input elements
 const rangeCharacters = document.getElementById('range-char');
 
@@ -12,7 +11,13 @@ const numbersEl = document.querySelector('#numbers');
 const symbolsEl = document.querySelector('#symbols');
 const upperCaseEl = document.querySelector('#uppercase');
 
-
+/* Inserting the Character Codes into arrays
+Character Cheatsheet -> https://www.petefreitag.com/cheatsheets/ascii-codes/
+*/
+const lowerCaseCharCodes = arrayLowToHigh(97, 122);
+const numberCharCodes = arrayLowToHigh(48, 57);
+const symbolCharCodes = arrayLowToHigh(33, 47).concat(58, 64).concat(91, 96).concat(123, 126);
+const upperCaseCharCodes = arrayLowToHigh(65, 90);
 
 //Synchronizing Range and Number Inputs
 rangeCharacters.addEventListener('input', syncCharAmount);
@@ -20,26 +25,36 @@ rangeCharacters.addEventListener('input', syncCharAmount);
 numberCharacters.addEventListener('input', syncCharAmount);
 
 function syncCharAmount(e) {
-    const ValueAmount = e.target.value;
-    rangeCharacters.value = ValueAmount;
-    numberCharacters.value = ValueAmount;
-};
+	const ValueAmount = e.target.value;
+	rangeCharacters.value = ValueAmount;
+	numberCharacters.value = ValueAmount;
+}
 
 //Generating the password when the form is submitted
+formContainer.addEventListener('submit', function (e) {
+	e.preventDefault();
 
-formContainer.addEventListener('submit', function(e) {
-    e.preventDefault();
+	const characterAmount = numberCharacters.value;
+	const includeUpperCase = upperCaseEl.value;
+	const includeNumbers = numbersEl.value;
+	const includeSymbols = symbolsEl.value;
 
-    const characterAmount = numberCharacters.value;
-    const includeUpperCase = upperCaseEl.value;
-    const includeNumbers = numbersEl.value;
-    const includeSymbols = symbolsEl.value;
+	const password = generatePassword(
+		characterAmount,
+		includeUpperCase,
+		includeNumbers,
+		includeSymbols
+	);
+});
 
-    const password = generatePassword(
+// looping over char code
 
-        characterAmount,
-        includeUpperCase,
-        includeNumbers,
-        includeSymbols
-    )
-})
+function arrayLowToHigh(low, high) {
+	let array = [];
+
+	for (let i = low; i <= high; i++) {
+		array.push(i);
+	}
+
+	return array;
+}
